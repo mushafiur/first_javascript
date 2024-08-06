371718efa964afc48c26e3db00f9d36dfb0381d7
MU-SHAFIUR RAHMAN <166214370+mushafiur@users.noreply.github.com>
2024-08-06 19:12:06 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// ex1
+function changeText(){
+	document.getElementById('change_t').innerHTML="it works bingo!";
+}
+// ex2
+function showText(){
+	document.getElementById('s_text').innerHTML="Its javascript not java both are diffrent."
+}
+// ex3
+function firstScript(){
+	document.getElementById('f_script').innerHTML="javascript"
+}
+// ex4
+function programmingLanguage(){
+	document.getElementById('p_language').innerHTML="programming language";
+}
+
+// ex6
+function hideContent(){
+	document.getElementById('hide').style.display="none";
+}
+
+// ex7
+function changeFontSize(){
+	document.getElementById('font_size').style.fontSize='45px';
+}
+
+// ex8
+function fontColor(){
+	document.getElementById('purple').style.color='purple';
+}
